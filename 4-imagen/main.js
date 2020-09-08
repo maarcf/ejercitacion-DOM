abfaeeee0559e7fdd591dfd1c7741502ccce572d
maarcf <marr.cairo@gmail.com>
2020-09-08 11:30:10 -0300

    agregue ej 4, me costo resolverlo

--- 4-imagen/main.js
+++ 4-imagen/main.js
@@ -6,3 +6,31 @@
 // chica	200px
 // mediana	500px
 // grande	800px
+
+const rtaTamanioFoto = prompt(`¿En que tamaño te gustaría ver la foto?
+Las opciones son:
+- Chica
+- Mediana
+- Grande`);
+
+const rtaNormalizada = rtaTamanioFoto.toLowerCase();
+
+const contenedorImg = document.querySelector('.contenedor-img');
+
+contenedorImg.classList.remove('chica', 'mediana', 'grande')
+
+const tamanioElegido = size => {
+  if (size === 'chica') {
+   contenedorImg.classList.add('chica')
+  }
+  else if (size === 'mediana') {
+   contenedorImg.classList.add('mediana')
+  }
+  else if (size === 'grande') {
+   contenedorImg.classList.add('grande')
+  }
+  else {
+    alert('No es un formato válido.')
+  }
+}
+tamanioElegido(rtaNormalizada)
